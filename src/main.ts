a4fc5e90d0b7e544b65a32fcefa04c5f7f009851
OhSeongHyeon <wert3976@gmail.com>
2026-01-04 03:04:27 +0900

    first commit

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+
+createApp(App).mount('#app')
+
+// env 테스트용 정크코드
+if ("development" === import.meta.env.MODE) {
+    console.log("MODE:", import.meta.env.MODE)
+    console.log("DEV:", import.meta.env.DEV)
+    console.log("ENV:", import.meta.env)
+    console.log("API:", import.meta.env.VITE_API_BASE_URL)
+}
+else {
+    console.log("모드 프로덕트인듯")
+}
+import { api } from './lib/api'
+let a = api.get("/api/health")
+a.then(r => console.log(r))
+
---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+export default defineConfig({
+  plugins: [vue()],
+  build: {
+    // outDir: "../mocktalkback/src/main/resources/static/front",
+    // emptyOutDir: true,
+  },
+  server: {  // Vite proxy 설정
+    proxy: {
+      // 프론트에서 /api 로 보내면 → 백엔드 8081으로 중계
+      "/api": {
+        target: "http://localhost:8081",
+        changeOrigin: true,
+      },
+    },
+  },
+})
